5ea07c66a1a19eb95176e18bc5e531c39fe3dd9d
Dittemor <eaa25dca@students.eaaa.dk>
2026-08-31 14:23:56 +0200

    Service folder created
    I created a service folder and a supabase.js file. Now I can import that code instead of having the same code on 3 different pages

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,12 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router";
-
-
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const headers = {
-  apikey: import.meta.env.VITE_SUPABASE_APIKEY,
-  "Content-Type": "application/json",
-};
+import { SUPABASE_URL, headers } from "../services/supabase";
 
 export default function EventPage() {
   const { eventId } = useParams();
--- src/pages/RegistrationsPage.jsx
+++ src/pages/RegistrationsPage.jsx
@@ -1,11 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
-
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const headers = {
-  apikey: import.meta.env.VITE_SUPABASE_APIKEY,
-  "Content-Type": "application/json"
-};
+import { SUPABASE_URL, headers } from "../services/supabase";
 
 export default function RegistrationsPage() {
   const [registrations, setRegistrations] = useState([]);
